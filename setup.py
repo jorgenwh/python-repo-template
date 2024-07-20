37704452da7be52bbfd2532be299a07059a8dce8
jorgenwh <jorgenwhen@gmail.com>
2024-07-20 19:22:47 +0200

    init

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(
+    name="example",
+    version="1.0.0",
+    author="Jørgen Henriksen",
+    description="",
+    long_description="",
+    packages=["example"],
+    zip_safe=False,
+)
